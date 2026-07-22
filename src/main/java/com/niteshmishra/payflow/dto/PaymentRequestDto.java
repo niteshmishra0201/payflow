850c02920ab339ad6f693fd5aa7a37b859aedb9d
Nitesh Mishra <nitesh.mishra0201@gmail.com>
2026-07-22 16:38:53 +0530

    Phase-3 started and Updated the paymentRequest dto

--- src/main/java/com/niteshmishra/payflow/dto/PaymentRequestDto.java
+++ src/main/java/com/niteshmishra/payflow/dto/PaymentRequestDto.java
@@ -1,5 +1,8 @@
 package com.niteshmishra.payflow.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import java.math.BigDecimal;
@@ -7,8 +10,17 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class PaymentRequestDto {
+
+    @NotNull(message = "merchantId is required")
     private Long merchantId;
+
+    @NotNull(message = "amount is required")
+    @DecimalMin(value = "0.01", message = "amount must be greater than zero")
     private BigDecimal amount;
+
+    @NotBlank(message = "currency is required")
     private String currency;
+
+    @NotBlank(message = "idempotencyKey is required")
     private String idempotencyKey;
 }
